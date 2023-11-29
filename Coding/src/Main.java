fcf272e8cfe3b7861d8af9bb75f25aeca8a134e5
cheolhyeon <tghy4@naver.com>
2023-11-29 19:36:17 +0900

    문제 URL : https://www.acmicpc.net/problem/status/2475 검증 수

--- Coding/src/Main.java
+++ Coding/src/Main.java
@@ -1,38 +1,21 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 단어 입력 받기
+        // 값을 입력받기 위한 Scanner 객체 생성
         Scanner sc = new Scanner(System.in);
-        String word = sc.nextLine().toUpperCase();
-        // 입력받은 단어를 배열로 나누기
-        char[] toArray = word.toCharArray();
 
-        Map<Character, Integer> map = new HashMap<>();
-        for (char c : toArray){
-            // 입력받은 단어의 인덱스 요소를 꺼내서 비교하기
-            // 겹치지 않을 경우 밸류 1, 겹칠경우 현재 밸류에서 +1
-            if (map.containsKey(c)) {
-                map.put(c, map.get(c)+1);
-            } else {
-                map.put(c, 1);
-            }
+        // 제곱 후의 값을 할당하기 위한 변수 선언
+        int square = 0;
+        // 값의 크기 만큼 순회할 for문
+        for (int i = 0; i < 5; i++) {
+            /// 값을 입력받아 제곱을 위해 값을 담을 변수 num 선언
+            int num = sc.nextInt();
+            // 제곱
+            square += num * num;
         }
-        // 가장 많이 겹친 문자를 꺼내기 위한 변수 선언
-        int maxCount = 0;
-        String maxChar = "";
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            // Map에 저장된 밸류의 값과 count 비교
-            if (entry.getValue() > maxCount) {
-                maxCount = entry.getValue();
-                maxChar = String.valueOf(entry.getKey());
-            } else if (entry.getValue() == maxCount) {
-                // 가장 많이 겹친 문자가 여러개일 경우 ?로 처리
-                maxChar = "?";
-            }
-        }
-        System.out.println(maxChar.toUpperCase());
+        // 결과에 담아 나누기 위한 변수 선언
+        int result = square % 10;
+        System.out.println(result);
     }
 }
