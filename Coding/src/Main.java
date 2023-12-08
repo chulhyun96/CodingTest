051baecbdbf9af7e6e68353de1e4da31fafb3965
cheolhyeon <tghy4@naver.com>
2023-12-08 19:14:34 +0900

    문제 URL :https://www.acmicpc.net/problem/8958 OX퀴즈

--- Coding/src/Main.java
+++ Coding/src/Main.java
@@ -4,36 +4,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        //반복 횟수 입력 받기
+        int iterationNum = Integer.parseInt(sc.nextLine());
 
-        while (true) {
+        // 반복회수 만큼 순회하면서 입력 받기
+        for (int i = 0; i < iterationNum; i++) {
             String line = sc.nextLine();
+            // 각 순회시마다 초기화
+            int total = 0;
+            int score = 0;
 
-            String[] lineSplit = line.split(" ");
-
-            String num1 = lineSplit[0];
-            String num2 = lineSplit[1];
-
-            int parseIntNum1 = Integer.parseInt(num1);
-            int parseIntNum2 = Integer.parseInt(num2);
-
-            String result;
-
-            //먼저 입력된 수가  0 0인지 검사 (종료를 위해)
-            if (parseIntNum1 == 0 && parseIntNum2 == 0) {
-                break;
+            // 값 비교해가며 값 할당
+            for (int j = 0; j < line.length(); j++) {
+                if (line.charAt(j) == 'O') {
+                    score++;
+                    total += score;
+                } else if (line.charAt(j) == 'X') {
+                    score = 0;
+                }
             }
-            // 약수
-            if (parseIntNum2 % parseIntNum1 == 0) {
-                result = "factor";
-            // 배수
-            } else if (parseIntNum1 % parseIntNum2 == 0) {
-                result = "multiple";
-            }
-            // 둘다 아닌 경우
-            else {
-                result = "neither";
-            }
-            System.out.println(result);
+            System.out.println(total);
         }
+        sc.close();
     }
 }
